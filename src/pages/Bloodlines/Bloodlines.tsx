b34bac647936c89037c299ac24e2e7b7ed784062
Gemdelle <milu200911@hotmail.com>
2025-07-07 19:48:55 -0300

    mastery path.

--- src/pages/Bloodlines/Bloodlines.tsx
+++ src/pages/Bloodlines/Bloodlines.tsx
@@ -381,7 +381,7 @@ const Bloodlines: React.FC = () => {
                     return accounts.map((account) => (
                       <div key={account.id} className={styles.row__account}>
                         {/* Account-specific data will go here */}
-                        <img src="/src/assets/images/masteries/mastery_0.png" alt="Mastery 0" className={styles.mastery__image} />
+                        <img src="/src/assets/images/masteries/mastery/mastery_0.png" alt="Mastery 0" className={styles.mastery__image} />
                       </div>
                     ));
                   })()}
