1a27bb00cde4ea4bce745e28f6815626f4144a32
Gemdelle <milu200911@hotmail.com>
2025-07-27 19:23:10 -0300

    cursor frame positioning.

--- src/pages/CursorSelection/CursorSelection.tsx
+++ src/pages/CursorSelection/CursorSelection.tsx
@@ -16,37 +16,37 @@ const CursorSelection: React.FC<CursorSelectionProps> = ({ onCursorSelected }) =
 
                 <div className={styles.cursorSelection__content}>
                     <div className={styles.cursorSelection__content__framesContainer}>
-                        <div className={styles.cursorSelection__content__frame}>
-                            <img src="/images/frames/cursor-frame-1.png" alt="Cursor Frame 1" />
-                            <div className={styles.cursorSelection__content__frameContent}>
-                                <img src="/images/cursors/cursor-default-1.png" alt="Cursor 1" />
-                                <img src="/images/cursors/cursor-pointer-1.png" alt="Cursor 2" />
+                        <div className={styles.cursorSelection__content__framesRow}>
+                            <div className={`${styles.cursorSelection__content__frame} ${styles.frame1}`}>
+                                <div className={styles.cursorSelection__content__frameContent}>
+                                    <img src="/images/cursors/cursor-default-1.png" alt="Cursor 1" />
+                                    <img src="/images/cursors/cursor-pointer-1.png" alt="Cursor 2" />
+                                </div>
+                            </div>
+                            <div className={`${styles.cursorSelection__content__frame} ${styles.frame2}`}>
+                                <div className={styles.cursorSelection__content__frameContent}>
+                                    <img src="/images/cursors/cursor-default-2.png" alt="Cursor 1" />
+                                    <img src="/images/cursors/cursor-pointer-2.png" alt="Cursor 2" />
+                                </div>
                             </div>
                         </div>
-                        <div className={styles.cursorSelection__content__frame}>
-                            <img src="/images/frames/cursor-frame-2.png" alt="Cursor Frame 2" />
-                            <div className={styles.cursorSelection__content__frameContent}>
-                                <img src="/images/cursors/cursor-default-2.png" alt="Cursor 1" />
-                                <img src="/images/cursors/cursor-pointer-2.png" alt="Cursor 2" />
+                        <div className={styles.cursorSelection__content__framesRow}>
+                            <div className={`${styles.cursorSelection__content__frame} ${styles.frame3}`}>
+                                <div className={styles.cursorSelection__content__frameContent}>
+                                    <img src="/images/cursors/cursor-default-3.png" alt="Cursor 1" />
+                                    <img src="/images/cursors/cursor-pointer-3.png" alt="Cursor 2" />
+                                </div>
                             </div>
-                        </div>
-                        <div className={styles.cursorSelection__content__frame}>
-                            <img src="/images/frames/cursor-frame-3.png" alt="Cursor Frame 3" />
-                            <div className={styles.cursorSelection__content__frameContent}>
-                                <img src="/images/cursors/cursor-default-3.png" alt="Cursor 1" />
-                                <img src="/images/cursors/cursor-pointer-3.png" alt="Cursor 2" />
-                            </div>
-                        </div>
-                        <div className={styles.cursorSelection__content__frame}>
-                            <img src="/images/frames/cursor-frame-4.png" alt="Cursor Frame 4" />
-                            <div className={styles.cursorSelection__content__frameContent}>
-                                <img src="/images/cursors/cursor-default-4.png" alt="Cursor 1" />
-                                <img src="/images/cursors/cursor-pointer-4.png" alt="Cursor 2" />
+                            <div className={`${styles.cursorSelection__content__frame} ${styles.frame4}`}>
+                                <div className={styles.cursorSelection__content__frameContent}>
+                                    <img src="/images/cursors/cursor-default-4.png" alt="Cursor 1" />
+                                    <img src="/images/cursors/cursor-pointer-4.png" alt="Cursor 2" />
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className={styles.cursorSelection__content__crystalPet}>
-                        {/* CrystalPet will be added here */}
+                        <img src="/images/pets/crystal-pet-3.png" alt="Crystal Pet" />
                     </div>
                 </div>
             </div>
