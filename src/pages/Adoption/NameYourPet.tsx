f57eb4e52fbaa16266d691cac66b69c7a7f5475f
Hunny <gonzalo.contogrobly@gmail.com>
2025-07-27 20:02:37 -0300

    Arregla el bug visual con el pet adoption.

--- src/pages/Adoption/NameYourPet.tsx
+++ src/pages/Adoption/NameYourPet.tsx
@@ -81,12 +81,13 @@ const NameYourPet: React.FC<NameYourPetProps> = ({ selectedEgg, onPetNamed, eggI
     };
 
     const handlePetClick = (event: React.MouseEvent) => {
-        playPetSound();
-
         // Only spawn heart if pet is fully hatched (not during hatching process)
         if (clickCount >= 4) {
             spawnHeart(event);
         }
+
+        // Play petting sound
+        playPetSound();
 
         // Trigger petting animation
         setIsPetting(true);
@@ -120,6 +121,10 @@ const NameYourPet: React.FC<NameYourPetProps> = ({ selectedEgg, onPetNamed, eggI
                 setTimeout(() => {
                     console.log('Starting pet fade in!');
                     setShowPetFadeIn(true);
+                    // Play pet sound at 70% of the fade-in animation (2.1 seconds later)
+                    setTimeout(() => {
+                        playPetSound();
+                    }, 2100);
                 }, 4000);
             }
         }
@@ -150,10 +155,10 @@ const NameYourPet: React.FC<NameYourPetProps> = ({ selectedEgg, onPetNamed, eggI
                                 transform: `scale(${0.7 + (Math.min(clickCount, 3) * 0.1)})`
                             }}
                         >
-                            {/* Pet image that appears under the egg from 3rd click */}
-                            {clickCount >= 3 && (
+                            {/* Pet image that appears under the egg when fade-in animation starts */}
+                            {showPetFadeIn && (
                                 <div
-                                    className={`${styles.pet__underneath} ${showPetFadeIn ? styles.fadeIn : ''} ${isPetting ? styles.petting : ''}`}
+                                    className={`${styles.pet__underneath} ${styles.fadeIn} ${isPetting ? styles.petting : ''}`}
                                     onLoad={() => console.log('Pet div rendered, showPetFadeIn:', showPetFadeIn)}
                                 >
                                     <img
