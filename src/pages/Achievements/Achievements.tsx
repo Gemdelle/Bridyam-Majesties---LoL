2d3f955c4093ad45af5f9a32da9ac744cf96f1c8
Gemdelle <milu200911@hotmail.com>
2025-07-29 01:29:42 -0300

    random level achievements.

--- src/pages/Achievements/Achievements.tsx
+++ src/pages/Achievements/Achievements.tsx
@@ -16,63 +16,63 @@ const achievements: Achievement[] = [
         description: "Take a champion from 0 to mastery 10",
         iconSrc: "/images/masteries/mastery/10.png",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 7
     },
     {
         name: "Skill Builder",
         description: "Level up champion masteries",
         iconSrc: "/images/masteries/mastery/level_plate.png",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 3
     },
     {
         name: "Battle Tested",
         description: "Play matches",
         iconSrc: "/images/ranked-btn/mission.png",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 9
     },
     {
         name: "Victory Seeker",
         description: "Win matches",
         iconSrc: "/images/ranked-btn/wins.png",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 5
     },
     {
         name: "First Blood",
         description: "Earn your first champion mastery",
         iconSrc: "/images/masteries/mastery/1.png",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 1
     },
     {
         name: "Rank Climber",
         description: "Advance to the next division",
         iconSrc: "/images/lol-elements/tier-challenger.webp",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 8
     },
     {
         name: "Tier Climber",
         description: "Ascend to a higher tier",
         iconSrc: "/images/lol-elements/tier-challenger-heml.webp",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 4
     },
     {
         name: "Majesty Collector",
         description: "Redeem majesty rewards",
         iconSrc: "/images/ranked-btn/porveldam.png",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 6
     },
     {
         name: "Victorious Warrior",
         description: "Win ranked games with victorious champions",
         iconSrc: "/images/ranked-btn/gladasmy.png",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 2
     },
     {
         name: "Carer",
@@ -86,14 +86,14 @@ const achievements: Achievement[] = [
         description: "Build friendships and social connections",
         iconSrc: "/images/achievement/achievement-11-1.png",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 3
     },
     {
         name: "Missions",
         description: "Complete various missions and challenges",
         iconSrc: "/images/achievement/achievement-12-1.png",
         progress: 100,
-        completedSteps: 10
+        completedSteps: 7
     }
 ];
 
